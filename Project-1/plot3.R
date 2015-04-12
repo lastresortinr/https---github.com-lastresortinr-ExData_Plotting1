69459340a81f52d224ccb0f641a38c7c16c428c7
lastresortinr <anuragchaplot@gmail.com>
2015-04-12 19:21:06 +0530

    Course Project 1

---
+++ Project-1/plot3.R
@@ -0,0 +1,10 @@
+setwd("E:/R/Exploratory Data Analysis")
+Data <- read.table("household_power_consumption.txt",skip=66600,nrow=3000,header=TRUE,sep=";")
+Data <- Data[Data[,1]=="1/2/2007" | Data[,1]=="2/2/2007",]
+x <- paste(Data[,1],Data[,2])
+Data[,1] <- as.Date(Data[,1],"%d/%m/%Y")
+Data$Time <- strptime(x,format="%d/%m/%Y %H:%M:%S")
+plot(Data$Time,Data[,7],type='l',ylab="Energy sub metering",xlab="")
+lines(Data$Time,Data[,8],col='orangered')
+lines(Data$Time,Data[,9],col='blue')
+legend("topright",lty=1,box.lwd=1,cex=0.75,col=c(1,"orangered","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
